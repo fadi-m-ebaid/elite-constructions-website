4bc6e5c0b878bf7a8d9cb1c5b95330f7ba64cc57
fadi-m-ebaid <fadi.micheal@hotmail.com>
2025-02-27 03:13:28 +0200

    Apply cashing for any static assets

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,20 @@ const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to complete even if there are ESLint errors.
     ignoreDuringBuilds: true,
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply caching to all static assets
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // 1-year caching
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
